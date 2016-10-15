94d19b24dbd94ae2d8fedd0e2adf9cdb859e7046
mixtv <mmmrvr@mail.ru>
2016-10-15 09:58:13 +0300

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+D1::Application.config.secret_token = 'dcf3fac1014c48b635e98a135f0bd4b6e2455db334457f228a4eda5a4b07f2e18d12556d0dab9350ab419b9afdc2dc56e412199f9b175f80af376a34c98dc39d'
